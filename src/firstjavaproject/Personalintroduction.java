14adba86a343e94e4f63fb36c7878ca7c88707f5
Richi <Richi@Snigdha.lan>
2026-05-26 22:15:51 +0530

    first comment

---
+++ src/firstjavaproject/Personalintroduction.java
@@ -0,0 +1,12 @@
+package firstjavaproject;
+
+public class Personalintroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" name:snigdha");
+System.out.println("college:silicon");
+System.out.println("btech ece");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module firstjavaproject {
+}
